5b4597f21a8fa996187910ca3c99019f700510e8
Annstella <annstellawangui@gmail.com>
2018-10-05 17:37:23 +0300

    created migrations

--- gram/models.py
+++ gram/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   first_name = models.CharField(max_length=30)
+   last_name = models.CharField(max_length=30)
+   bio = models.CharField(max_length=200)
+   profile_pic = models.ImageField(upload_to='profile/')
+   
+
 class Image(models.Model):
    image = models.ImageField(upload_to='images/')
    caption = models.TextField(blank=True)
    likes = models.PositiveIntegerField(default=0)
    comment = models.CharField(max_length=200)
-   profile = models.ForeignKey(profile, on_delete=models.CASCADE)
+   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
