9ad3c8d56d5456538c30efc33b71b6a787171e8b
Annstella <annstellawangui@gmail.com>
2018-10-05 17:06:08 +0300

    creating image models

--- gram/models.py
+++ gram/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class NewsLetterRecipients(models.Model):
-    name = models.CharField(max_length = 30)
-    email = models.EmailField()
+class Image(models.Model):
+   image = models.ImageField(upload_to='images/')
+   caption = models.TextField(blank=True)
+   likes = models.PositiveIntegerField(default=0)
+   comment = models.CharField(max_length=200)
+   profile = models.ForeignKey(profile, on_delete=models.CASCADE)
