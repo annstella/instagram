a09afbb553fbec2d032ec5f0590a03e0621a3a90
Annstella <annstellawangui@gmail.com>
2018-10-05 15:53:50 +0300

    created emails field

--- gram/views.py
+++ gram/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render,redirect
+from .forms import NewsLetterForm
 # Create your views here.
 def welcome(request):
    return render(request, 'welcome.html')
 
-# def gram(request):
-#    gram = Image.objects.all()
-
-#    return render(request, 'all-gram/today-gram.html', {"gram":gram})
+def gram_today(request):
+#........
+    if request.method == 'POST':
+        form = GramLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = GramLetterForm()
+    return render(request, 'all-gram/today-gram.html', {gram":gram,"letterForm":form})
