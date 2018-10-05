b8c7093e69243c86b70280872f35b5ba2ebd52b3
Annstella <annstellawangui@gmail.com>
2018-10-05 16:31:47 +0300

    subscibers getting a  welcome email

--- gram/views.py
+++ gram/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-from .forms import NewsLetterForm
+from .forms import GramLetterForm
+from .email import send_welcome_email
 # Create your views here.
 def welcome(request):
    return render(request, 'welcome.html')
 
 def gram_today(request):
-#........
     if request.method == 'POST':
         form = GramLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
-    else:
-        form = GramLetterForm()
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = GramLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('gram_today')
+            #.................
     return render(request, 'all-gram/today-gram.html', {gram":gram,"letterForm":form})
