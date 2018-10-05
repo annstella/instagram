ee1b578ff3a2457a0ecc54c1be7e95ba523595e9
Annstella <annstellawangui@gmail.com>
2018-10-05 15:58:58 +0300

    validating a form instance the values of the form

--- gram/views.py
+++ gram/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .forms import NewsLetterForm
 # Create your views here.
 def welcome(request):
