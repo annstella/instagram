87a49b6faf0e05abb7bc85e8c056d69d01d46e38
Annstella <annstellawangui@gmail.com>
2018-10-06 16:11:37 +0300

    changed url patterns

--- gram/models.py
+++ gram/models.py
@@ -4,47 +4,50 @@ import datetime as dt
 
 # Create your models here.
 class Profile(models.Model):
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   first_name = models.CharField(max_length=30)
-   last_name = models.CharField(max_length=30)
-   bio = models.CharField(max_length=200)
-   profile_pic = models.ImageField(upload_to='profile/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200)
+    profile_pic = models.ImageField(upload_to='profile/')
 
-def __str__(self):
+    def __str__(self):
        return self.first_name
 
-def save_profile(self):
-       self.save()
+    def save_profile(self):
+        self.save()
 
-def delete_profile(self):
-       self.delete()
+    def delete_profile(self):
+        self.delete()
 
-@classmethod
-def get_profile(cls):
-    profiles = cls.objects.all()
-    return profiles
+    @classmethod
+    def get_profile(cls):
+        profiles = cls.objects.all()
+        return profiles
 
-@classmethod
-def search_profile(cls, query):
-    profile = cls.objects.filter(user__username__icontains=query)
-    return profile
+    @classmethod
+    def search_profile(cls, query):
+        profile = cls.objects.filter(user__username__icontains=query)
+        return profile
    
 
 class Image(models.Model):
-   image = models.ImageField(upload_to='images/')
-   caption = models.TextField(blank=True)
-   likes = models.PositiveIntegerField(default=0)
-   comment = models.CharField(max_length=200)
-   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    caption = models.TextField(blank=True)
+    likes = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-def save_images(self):
-    self.save()
+    def __str__(self):
+        return self.caption
 
-def total_likes(self):
-    return self.likes.count()
+    def save_images(self):
+        self.save()
 
-def save_comment(self):
-    self.save()
+    def total_likes(self):
+        return self.likes.count()
+
+    def save_comment(self):
+        self.save()
 
 
 class Follow(models.Model):
--- gram/views.py
+++ gram/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-from .models import Image
+from .models import Profile , Image
 
 from .email import send_welcome_email
 # Create your views here.
 def welcome(request):
-   return render(request, 'welcome.html')
+    images = Image.objects.all()
+
+    context = {
+        'images':images , 
+    }
+    return render(request, 'welcome.html', context)
+
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gram/search.html',{"message":message,"profiles": searched_profiles})
+
+    else:
+        message = "no term matches your search"
+        return render(request, 'all-gram/search.html',{"message":message})
 
